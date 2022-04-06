4e8640a4eb01b127d30616d1b61608ddfd428f55
svtter <svtter@qq.com>
2022-04-06 20:37:42 +0800

    feat: add parse_str

--- builder/entity/version.py
+++ builder/entity/version.py
@@ -7,5 +7,18 @@ class Version:
     y: int
     z: int
 
+    @classmethod
+    def parse_str(cls, content: str) -> 'Version':
+        start = 0
+        v = []
+        for i in range(len(content)):
+            if content[i] == '.':
+                v.append(int(content[start:i]))
+                start = i + 1
+
+        v.append(int(content[start:]))
+        assert len(v) == 3
+        return Version(v[0], v[1], v[2])
+
     def get_full(self, split=".") -> str:
         return f"v{self.x}{split}{self.y}{split}{self.z}"
---
+++ builder/tests/test_parse.py
@@ -0,0 +1,7 @@
+from builder.entity.version import Version
+
+
+def test_version_parse():
+    content = '0.11.0'
+    v = Version.parse_str(content=content)
+    assert v.get_full() == 'v0.11.0'
