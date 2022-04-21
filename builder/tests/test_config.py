0878768496c31b2aa87b6cd256776c0d6cf6a109
svtter <svtter@qq.com>
2022-04-21 11:30:14 +0800

    feat: remove config.yml

--- builder/tests/test_config.py
+++ builder/tests/test_config.py
@@ -4,3 +4,4 @@ from ..conf import Config, Version
 def test_load_config():
     c: Config = Config.load_config()
     assert c.get_version() == Version(0, 1, 0)
+    assert c.prefix == 'qms-compose'
