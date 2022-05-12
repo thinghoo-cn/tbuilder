3a9a481ea279a1a4fa3ebead79ed60a7535880c6
svtter <svtter@qq.com>
2022-05-12 17:41:14 +0800

    feat: add http server

--- builder/__main__.py
+++ builder/__main__.py
@@ -1,14 +1,22 @@
 import argparse
+from ast import arg
+from tracemalloc import start
+from xml.etree.ElementInclude import default_loader
 from .core.conf import current_repo, logger, CONFIG
 from .core.version import show_hash
 from .core.hash_checker import HashChecker
 from .core.images import build as image_build, save_image
+from .core.httpserver import start_http
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='tbuilder is an application to build image.')
-    parser.add_argument('cmd', choices=['check', 'build', 'save', 'gen', 'show'],
+    parser.add_argument('cmd', choices=['check', 'build', 'save', 'gen', 'show', 'http'],
                         help='select one command to run.')
+    parser.add_argument('--username', type=str, help='http server username')
+    parser.add_argument('--password', type=str, help='http server password')
+    parser.add_argument('--port', type=int, help='http server port.')
+
     args = parser.parse_args()
 
     checker = HashChecker(current_repo)
@@ -23,3 +31,6 @@ if __name__ == '__main__':
         CONFIG.gen()
     elif args.cmd == 'show':
         logger.info(CONFIG.repo_list)
+    elif args.cmd == 'http':
+        start_http(USERNAME=args.username, PASSWORD=args.password, port=args.port)
+
---
+++ builder/core/httpserver.py
@@ -0,0 +1,51 @@
+"""
+search "python http server with basic auth"
+simplified from this gist
+https://gist.github.com/mauler/593caee043f5fe4623732b4db5145a82
+"""
+
+import http.server
+import socketserver
+import base64
+
+
+def get_handler(AUTH_KEY):
+    class BasicAuthHandler(http.server.SimpleHTTPRequestHandler):
+
+        def do_HEAD(self):
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+
+        def do_AUTHHEAD(self):
+            self.send_response(401)
+            self.send_header("WWW-Authenticate", 'Basic realm="FileServer"')
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+
+        def do_GET(self):
+            """ Present frontpage with user authentication. """
+            if self.headers.get("Authorization") == None:
+                self.do_AUTHHEAD()
+                self.wfile.write(b"no auth header received")
+            elif self.headers.get("Authorization") == "Basic " + AUTH_KEY:
+                super().do_GET()
+            else:
+                self.do_AUTHHEAD()
+                self.wfile.write(self.headers.get("Authorization").encode())
+                self.wfile.write(b"not authenticated")
+    return BasicAuthHandler
+
+
+class ThreadingHTTPServer(socketserver.ThreadingMixIn, http.server.HTTPServer):
+    daemon_threads = True
+
+
+def start_http(USERNAME, PASSWORD, port):
+    AUTH_KEY = base64.b64encode('{}:{}'.format(USERNAME, PASSWORD).encode()).decode()
+    address = ('', port)
+
+    handler = get_handler(AUTH_KEY=AUTH_KEY)
+    print('server listening at', address)
+    with ThreadingHTTPServer(address, handler) as httpd:
+        httpd.serve_forever()
