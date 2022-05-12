f9b66683981a8766359185bc7cc942932bf0c19f
svtter <svtter@qq.com>
2022-05-12 09:07:26 +0800

    fix: fix --no-cache

--- builder/core/images.py
+++ builder/core/images.py
@@ -12,7 +12,7 @@ def build():
         with c.cd(r.folder):
             full_image_name = f"{CONFIG.get_prefix()}_{r.image}"
             key_param = f'--build-arg ssh_prv_key="$(cat {CONFIG.key_file})"'
-            cache_opt = '' if CONFIG.cache else 'no-cache'
+            cache_opt = '' if CONFIG.cache else '--no-cache'
             if r.key:
                 full_cmd = f"docker build {cache_opt} {key_param} -t {full_image_name}:{CONFIG.get_version().get_full()} ."
             else:
